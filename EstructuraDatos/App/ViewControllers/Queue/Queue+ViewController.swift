d36d32ffbba2036ee841867e2389279be40ac9bc
Vicwell28 <vicwell.bj@gmail.com>
2023-01-06 13:20:20 -0600

    scaffolding and viewscontrollers added

---
+++ EstructuraDatos/App/ViewControllers/Queue/Queue+ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Queue+ViewController.swift
+//  EstructuraDatos
+//
+//  Created by soliduSystem on 06/01/23.
+//
+
+import UIKit
+
+class Queue_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
